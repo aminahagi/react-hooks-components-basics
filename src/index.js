588b8de3f92fc680597a8410fb2a8cd9a2e50260
aminahagi <amina.hagi@student.moringaschool>
2023-01-10 17:42:03 +0300

    cmplete

--- src/index.js
+++ src/index.js
@@ -3,13 +3,3 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-// src/Article.js
-function Article() {
-    return (
-      <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
-    );
-  }
-  function Comment() {
-    return <div>Naturally, I agree with this article.</div>;
-  }
- 
